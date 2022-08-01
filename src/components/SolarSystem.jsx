1bdd54ee7f587457ef885f5ed00050aa8944d028
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-08-01 10:36:49 -0300

    feat: ensure component SolarSystem are rendered on success

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class SolarSystem extends Component {
+  render() {
+    return <div data-testid="solar-system">SolarSystem</div>;
+  }
+}

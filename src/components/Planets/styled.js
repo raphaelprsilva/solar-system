fbb3c7bf21cb4aad0c2a4175980d2cd74d399e28
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-08-02 13:59:56 -0300

    styles: change flex-direction

--- src/components/Planets/styled.js
+++ src/components/Planets/styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const PlanetsWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   width: 100%;
 `;

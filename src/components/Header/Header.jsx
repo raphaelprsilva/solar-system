82cd22dd338ee1fa47a7ea82b3e488ce5a53741e
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-08-02 13:25:59 -0300

    style: add Header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import * as S from './styled';
+
 export default class Header extends Component {
   render() {
     return (
-      <header>
+      <S.HeaderWrapper>
         <h1>Sistema Solar</h1>
-      </header>
+      </S.HeaderWrapper>
     );
   }
 }
---
+++ src/components/Header/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.header`
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+  color: white;
+  background-color: #192734;
+  border-bottom: 1px solid #38444d;
+`;

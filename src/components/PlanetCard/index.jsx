599223fd52d0926f0cb66dc37d4db92958d44246
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-08-03 13:03:51 -0300

    style: add PlanetCard style

--- src/components/PlanetCard/index.jsx
+++ src/components/PlanetCard/index.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import * as S from './styled';
+
 export default class PlanetCard extends Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
-        <p data-testid="planet-name">{ planetName }</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
-      </div>
+      <S.PlanetCardWrapper data-testid="planet-card">
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={planetImage} alt={`Planeta ${planetName}`} />
+      </S.PlanetCardWrapper>
     );
   }
 }
---
+++ src/components/PlanetCard/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const PlanetCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  padding: 0.2rem 0.5rem;
+`;
